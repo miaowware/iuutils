2d33bc5e0ab34c9ff75b752d47ba6eb6da6f02f8
0x5c <dev@0x5c.io>
2021-10-13 15:50:13 -0400

    Checkpoint commit

--- src/iu.rs
+++ src/iu.rs
@@ -1,29 +1,102 @@
-use nix::unistd::{
-    getresuid,
-    getresgid,
-    setresuid,
-    setresgid,
-}; 
-use std::process::Command;
+use nix::unistd::{ResGid, ResUid, getresgid, getresuid, setresgid, setresuid}; 
+use std::process::{self, Command};
 use std::os::unix::process::CommandExt;
 use std::path::Path;
+use std::env::args;
+use anyhow;
 
+#[allow(unreachable_code)]
 
 fn main() {
+    let mut args = args();
+
+    // Discarding the first arg
+    println!("a_0: {:?}", args.next());
+
+    let subcommand = match args.next() {
+        Some(s) => SubCommand::from(s),
+        None => SubCommand::Invalid(String::from("wth!!!")),
+    };
+
+    println!("cmd: {:?}", subcommand);
+    println!("args: {:?}", args);
+
+
+
+    process::exit(0);
+
+
     let id_path = Path::new("/usr/bin/id");
 
-    let resuid = getresuid().unwrap();
-    let resgid = getresgid().unwrap();
+    let (resuid, resgid) = match get() {
+        Ok(i) => i,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        },
+    };
 
-    let effuid = resuid.effective;
-    let effgid = resgid.effective;
 
     println!("{:?}", resuid);
     println!("{:?}", resgid);
 
-    // Set all IDs to desired values
-    setresgid(effgid, effgid, effgid).unwrap();
-    setresuid(effuid, effuid, effuid).unwrap();
+    match drop(resuid, resgid) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        },
+    }
 
     Command::new(id_path).exec();
+}
+
+fn get() -> anyhow::Result<(ResUid, ResGid)> {
+    Ok((getresuid()?, getresgid()?))
+}
+
+fn drop(ru: ResUid, rg: ResGid) -> anyhow::Result<()> {
+    let eu = ru.effective;
+    let eg = rg.effective;
+
+    // Set all IDs to desired values
+    setresgid(eg, eg, eg)?;  // Dropping group first to ensure dropping the user works
+    setresuid(eu, eu, eu)?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+enum SubCommand {
+    List,
+    Copy,
+    Move,
+    Remove,
+    Link,
+    Makedir,
+    Touch,
+    Help,
+    Version,
+    Debug,
+    None,
+    Invalid(String),
+}
+
+impl From<String> for SubCommand {
+    fn from(s: String) -> Self {
+        match &s as &str {
+            "ls" | "list" => Self::List,
+            "cp" | "copy" => Self::Copy,
+            "mv" | "move" => Self::Move,
+            "rm" | "remove" => Self::Remove,
+            "ln" | "link" => Self::Link,
+            "mkdir" => Self::Makedir,
+            "touch" => Self::Touch,
+            "help" | "--help" | "-h" => Self::Help,
+            "version" | "--version" | "-V" => Self::Version,
+            "debug" => Self::Debug,
+            "" => Self::None,
+            _ => Self::Invalid(s),
+        }
+    }
 }
